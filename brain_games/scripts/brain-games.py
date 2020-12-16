4e62b936a9a0a52025fb091c59c55bd928964d29
Sergey Kornienko <skornienko29@gmail.com>
2020-12-16 15:16:08 +0300

    rename brain-games.py

--- brain_games/scripts/brain-games.py
+++ brain_games/scripts/brain-games.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from brain_games.cli import welcome_user
+
 def intro(greet):
     print(greet)
 
@@ -8,4 +10,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
+    welcome_user()

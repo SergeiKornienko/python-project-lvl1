cde6f746780ea23ab8cb7d2080f8afbb3be3a04b
Sergey Kornienko <skornienko29@gmail.com>
2020-12-18 21:58:44 +0300

    fix bags of linter

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,12 @@
+"""introduction."""
+
 import prompt
 
+"""intro function"""
+
+
 def welcome_user():
+
     name = prompt.string('May I have your name? ')
+
     print('Hello, {}!'.format(name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
-from brain_games.cli import welcome_user
+from brain_games import welcome_user
+
 
 def intro(greet):
     print(greet)
 
+
 def main():
     intro('Welcome to the Brain Games!')
     welcome_user()
+
 
 if __name__ == '__main__':
     main()

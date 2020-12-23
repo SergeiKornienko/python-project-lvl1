1f546f439c5b0da3714bbf35b662a924437d83bb
Sergey Kornienko <skornienko29@gmail.com>
2020-12-23 11:26:02 +0300

    mod brain_games/scripts/brain_even.py makefile pyproject.toml

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 """Scripts for power brain-even."""
 
-from brain_games import game_even
+from brain_games import game_summon
 
 
 def main():
     """Power game_even()."""
-    game_even.game_even()
+    game_summon.game_summon('even')
 
 
 if __name__ == '__main__':

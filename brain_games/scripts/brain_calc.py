5d6e041e810b010d0fe75ce641b2de456dddcb24
Sergey Kornienko <skornienko29@gmail.com>
2020-12-23 17:19:55 +0300

    add brain_calc.py

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""Scripts for power brain-calc."""
+
+from brain_games import game_summon
+
+
+def main():
+    """Power game_calc()."""
+    game_summon.game_summon('calc')
+
+
+if __name__ == '__main__':
+    main()

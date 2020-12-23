d6268aea461fa555baef13615b30183a7322e531
Sergey Kornienko <skornienko29@gmail.com>
2020-12-23 17:13:07 +0300

    mod brain_games/game_summon.py game_even.py

--- brain_games/game_summon.py
+++ brain_games/game_summon.py
@@ -1,10 +1,14 @@
-"""Main module of game brain-even."""
+"""Main module of brain-games."""
 
 from brain_games import cli
 from brain_games.games import game_even
 
+
 def game_summon(name_game):
-    """Power game brain-even.
+    """Power games.
+
+    Args:
+        name_game: str
 
     Returns:
         Return query to player.
@@ -12,21 +16,25 @@ def game_summon(name_game):
     print('Welcome to the Brain Games!')  # noqa: WPS421
     name = cli.welcome_user('May I have your name? ')
     print('Hello, {a}!'.format(a=name))  # noqa: WPS421
-    print((game_even.game_even())[0])
-    
+    print((game_even.game_even())[0])  # noqa: WPS421
     index = 0
     while index < 3:
-        (intro, question, true_answer) = game_even.game_even()
-        answer = cli.welcome_user('Question: {a}\nYour answer: '.format(a=question))  # noqa: WPS421
+        (question, true_answer) = (game_even.game_even())[1]
+        answer = cli.welcome_user((
+            'Question: {a}\n'
+            +
+            'Your answer: '
+            ).format(a=question),
+        )
         if answer == true_answer:
             print('Correct!')  # noqa: WPS421
         else:
             print(  # noqa: WPS421
                 ("'{a}' is wrong answer ;). Correct answer was '{b}'."
-                ).format(a=answer, b=true_answer),
+                 ).format(a=answer, b=true_answer),
                 )
             return print(  # noqa: WPS421
-            "Let's try again, {a}!".format(a=name),
+                "Let's try again, {a}!".format(a=name),
             )
         index += 1
     return print(  # noqa: WPS421

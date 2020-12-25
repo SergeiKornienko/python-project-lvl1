8a5ba0922d34adc247d208948eb1ec1deba32203
Sergey Kornienko <skornienko29@gmail.com>
2020-12-25 13:50:19 +0300

    fix hexlet-check in launcher_game.py

--- brain_games/launcher_game.py
+++ brain_games/launcher_game.py
@@ -27,11 +27,11 @@ def launch_game(game):
             print(  # noqa: WPS421
                 ("'{a}' is wrong answer ;). Correct answer was '{b}'."
                  ).format(a=answer, b=true_answer),
-                )
+                  )
             return print(  # noqa: WPS421
                 "Let's try again, {a}!".format(a=player_name),
             )
         index += 1
     return print(  # noqa: WPS421
         'Congratulations, {a}!'.format(a=player_name),
-        )
+                 )

8941a4d59e3b0cc1ec251feeeeef7bb686820c2b
SergeyKornienko <skornienko29@mail.com>
2020-12-31 18:32:30 +0300

    mod brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -4,19 +4,9 @@
 from brain_games import cli
 
 
-def intro(greet):
-    """Take sring and print.
-
-    Args:
-        greet: str
-
-    """
-    print(greet)  # noqa:WPS421
-
-
 def main():
     """Print greeting."""
-    intro('Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')  # noqa:WPS421
     name = cli.welcome_user('May I have your name? ')
     print('Hello, {a}!'.format(a=name))  # noqa:WPS421
 
